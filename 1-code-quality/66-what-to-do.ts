b9fd1e75a5385c07647095b27faf4891ca2c56ae
Markel Tuzynskyi <mtuzyn@softserveinc.com>
2020-07-18 09:22:53 +0300

    update

---
+++ 1-code-quality/66-what-to-do.ts
@@ -0,0 +1,31 @@
+1. Make a Styleguide for your team
+2. Make a Glossary of Domain Terms for your team
+3. Involve entire team in Code Reviews
+4. Configure Linter and Formatter
+5. Use EditorConfig to share the coding style across different IDEs
+6. Always K.I.S.S.
+7. Know your technology
+
+
+
+
+
+
+
+// FP / FRP
+https://www.youtube.com/watch?v=2CKvxbdutxg
+https://www.youtube.com/watch?v=XRLBopMvmAQ
+https://medium.com/javascript-scene/composing-software-the-book-f31c77fc3ddc
+https://www.manning.com/books/functional-programming-in-javascript
+
+// OOP
+https://medium.com/javascript-scene/common-misconceptions-about-inheritance-in-javascript-d5d9bab29b0a
+https://medium.com/javascript-scene/the-two-pillars-of-javascript-ee6f3281e7f3
+https://medium.com/@cscalfani/goodbye-object-oriented-programming-a59cda4c0e53
+https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/this%20&%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes
+
+// Components
+https://medium.com/@jtomaszewski/how-to-write-good-composable-and-pure-components-in-angular-2-1756945c0f5b
+
+// DDD
+https://www.angulararchitects.io/aktuelles/sustainable-angular-architectures-1/
