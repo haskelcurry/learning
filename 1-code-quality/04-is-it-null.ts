00792c3b5ef09bb662bcd589234d4cc1fdc2b030
Markel Tuzynskyi <mtuzyn@softserveinc.com>
2020-11-06 12:42:29 +0200

    update

--- 1-code-quality/04-is-it-null.ts
+++ 1-code-quality/04-is-it-null.ts
@@ -1,7 +1,18 @@
-const id = patient.id || null;
+const access = patient.isAdmin || null;
 
-
-// Three variants instead of two:
-// true, false, null
+// Four variants instead of three:
+// true, false, undefined, null
 //
 // Developer: "What is the reason for this?"
+
+
+
+
+
+const diagnosis = patient.diagnosis || null;
+
+// The variants are:
+// any, undefined, null
+//
+// Why is null for? To signalize that it's loaded but it's empty?
+// Really?
